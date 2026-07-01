e2518ba14330a32e8d894877a3e0243203d69117
Alucardnet <hydradevsec98@gmail.com>
2026-07-01 12:47:16 -0600

    Validar usuario y contraseña en la BD

--- Controllers/Login.php
+++ Controllers/Login.php
@@ -4,6 +4,7 @@ class Login extends Controllers
 {
     public function __construct()
     {
+        session_start();
         parent::__construct();
     }
 
@@ -20,7 +21,30 @@ class Login extends Controllers
 
     public function loginUser()
     {
-        dep($_POST);
+        //dep($_POST);
+
+        if ($_POST) {
+            if (empty($_POST['txtEmail']) || empty($_POST['txtPassword'])) {
+                $arrResponse = array('status' => false, 'msg' => 'Error de datos');
+            } else {
+                $strUsuario = strtolower(strClean($_POST['txtEmail']));
+                $strPassword = hash("SHA256", $_POST['txtPassword']);
+                $requestUser = $this->model->loginUser($strUsuario, $strPassword);
+                if (empty($requestUser)) {
+                    $arrResponse = array('status' => false, 'msg' => 'El usuario o la contraseña es incorrecto.');
+                } else {
+                    $arrData = $requestUser;
+                    if ($arrData['status'] == 1) {
+                        $_SESSION['idUser'] = $arrData['idpersona'];
+                        $_SESSION['login'] = true;
+                        $arrResponse = array('status' => true, 'msg' => 'ok');
+                    } else {
+                        $arrResponse = array('status' => false, 'msg' => 'Usuario inactivo');
+                    }
+                }
+            }
+            echo json_encode($arrResponse, JSON_UNESCAPED_UNICODE);
+        }
         die();
     }
 }
